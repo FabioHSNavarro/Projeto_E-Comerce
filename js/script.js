8ef327b8c5d561aa20579e3b1e90c6f670ab54bf
Fabio H. S. Navarro <fabioanhaia16@gmail.com>
2026-03-01 08:27:26 -0300

    v1.3 - Troquei a API DummyJson pela minha propria API, Link:https://api-e-commerce-ojub.onrender.com/produtos (alteracoes visiveis apenas no console.log)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const URL = "https://dummyjson.com/products";
+const URL = "https://api-e-commerce-ojub.onrender.com/produtos";
 
 async function ChamarApi(){
-    const resp = await fetch(URL);
+    const resp = await fetch (URL);
     console.log(resp);
     if (resp.status === 200){
         const obj = await resp.json();
